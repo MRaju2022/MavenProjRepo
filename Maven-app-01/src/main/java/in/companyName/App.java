2cda50c58df77886e28858e7622e4b465a961546
Raju <raju@gmail.com>
2024-06-03 19:45:12 +0530

    Task 1001 changes

--- Maven-app-01/src/main/java/in/companyName/App.java
+++ Maven-app-01/src/main/java/in/companyName/App.java
@@ -10,10 +10,15 @@ public class App
     {
         System.out.println( "Hello World!" );
 		System.out.println("Hello! Welcome to java classes");
-		
+
 		//Code changes related to task:1002
 		int a = 100;
 		int b = 200;
 		System.out.println("The sum of given 2 numbers is:" +(a+b))
+
+		//Code changes for 1001 task
+		System.out.println("Welcome to Spring boot classes");
+		System.out.println("Welcome to Microservices classes");
+
     }
 }
