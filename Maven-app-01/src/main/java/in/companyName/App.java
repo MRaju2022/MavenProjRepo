c94845242ac267e72978413532d61fa60562938e
Raju <raju@gmail.com>
2024-06-03 19:42:01 +0530

    1002 changes

--- Maven-app-01/src/main/java/in/companyName/App.java
+++ Maven-app-01/src/main/java/in/companyName/App.java
@@ -10,5 +10,10 @@ public class App
     {
         System.out.println( "Hello World!" );
 		System.out.println("Hello! Welcome to java classes");
+		
+		//Code changes related to task:1002
+		int a = 100;
+		int b = 200;
+		System.out.println("The sum of given 2 numbers is:" +(a+b))
     }
 }
